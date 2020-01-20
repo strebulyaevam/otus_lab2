e1dc39a9b5bc91a1439eaccd3d524dc419c0b7e2
Maria <mstrebulyaeva@gmail.com>
2020-01-20 08:59:16 +0300

    Create Factory Class

---
+++ src/test/java/testhomepage/OtusHome.java
@@ -0,0 +1,38 @@
+package testhomepage;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class OtusHome {
+
+    private static Logger Log = LogManager.getLogger(OtusHome.class);
+
+    WebDriver driver;
+
+    public OtusHome (WebDriver driver) {this.driver = driver;}
+
+    By loc_main_courses = By.xpath("//div[@class='subtitle-new' and text()='Популярные курсы']");
+
+    public boolean findMainCourses()
+    {
+        WebElement mainCoursesEl;
+
+        try {
+            Log.info("Try to find mainCoursesEl");
+
+            mainCoursesEl = (new WebDriverWait(driver, 10))
+                    .until(ExpectedConditions.presenceOfElementLocated(loc_main_courses));
+        } catch (Exception e) {
+            Log.error("Error - mainCoursesElement was not found at the opened page", e);
+            return false;
+        }
+        Log.info("mainCoursesEl was found successfully");
+
+        return true;
+    }
+}
---
+++ src/test/java/testhomepage/WebDriverFactory.java
@@ -0,0 +1,67 @@
+package testhomepage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.Assert;
+
+public class WebDriverFactory {
+
+    private static Logger Log = LogManager.getLogger(WebDriverFactory.class);
+
+    enum Browsers {
+        CHROME,
+        FIREFOX
+    }
+
+    public static WebDriver createNewDriver(String webDriverName) throws Exception{
+        return createNewDriver(webDriverName, null);
+    }
+
+
+    public static WebDriver createNewDriver(String webDriverName, MutableCapabilities options) throws Exception {
+        WebDriver driver=null;
+        Browsers browser;
+
+        try {
+            Log.info("Try to set up browser " + webDriverName);
+            browser = Browsers.valueOf(webDriverName.toUpperCase());
+            Log.info(webDriverName + " is possible browser");
+        } catch (Exception e) {
+            Log.error(webDriverName + " is NOT possible browser");
+            throw e;
+        }
+
+        switch (browser) {
+            case CHROME: {
+                WebDriverManager.chromedriver().setup();
+                if (options!=null) {
+                    driver = new ChromeDriver((ChromeOptions) options);
+                }
+                else{
+                    driver = new ChromeDriver();
+                }
+                Log.info("Browser " + webDriverName + " set up successfully");
+                break;
+            }
+            case FIREFOX: {
+                WebDriverManager.firefoxdriver().setup();
+                if (options!=null) {
+                    driver = new FirefoxDriver((FirefoxOptions) options);
+                }
+                else{
+                    driver = new FirefoxDriver();
+                }
+                Log.info("Browser " + webDriverName + " set up successfully");
+                break;
+            }
+        }
+        return driver;
+    }
+}
